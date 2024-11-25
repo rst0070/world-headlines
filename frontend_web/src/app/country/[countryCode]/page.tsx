0ebde1178a48d1b6f784b957c0295fe330c53e92
Wonbin Kim <kwb0711@gmail.com>
2024-11-25 14:37:37 +0900

    Structure frontend_web - better for using backend

--- frontend_web/src/app/country/[countryCode]/page.tsx
+++ frontend_web/src/app/country/[countryCode]/page.tsx
@@ -2,46 +2,20 @@ import Image from "next/image";
 import { NewsArticle } from "@/types/NewsArticle";
 import { CountryHeadline } from "@/types/CountryHeadline";
 import { ReactNode } from "react";
+import NewsItem from "@/components/NewsItem";
 import {getHeadlineInfo, getNewsArticles} from "@/services/api/country"
 
 
-/**
- * 
- * @param article 
- * @returns 
- */
-function getNewsArticleElement(newsArticle:NewsArticle, noImageUrl:string): ReactNode {
+export default async function Page(props: { params: Promise<{ countryCode: string }> }) {
 
-    let image_tag = <></>;
-    if(newsArticle.imageUrl != noImageUrl)
-        image_tag = <img src={newsArticle.imageUrl} alt="" width={280} height={168}/>;
-
-    return (
-        <div className="news-item" key={newsArticle.url + newsArticle.title}>
-            {image_tag}
-            <div className="news-details">
-                <h3><a href={newsArticle.url} target="_blank">{newsArticle.title}</a></h3>
-                <p className="article-publish-date">{newsArticle.publishDate}</p>
-                <p>{newsArticle.description}</p>
-                <ul className="sources">
-                    <li><a href="#">{newsArticle.source}</a></li>
-                </ul>
-            </div>
-        </div>
-    );
-}
-
-
-export default async function Page({ params }: { params: { countryCode: string } }) {
-
-    const countryCode:string                = params.countryCode
-    const newsArticleList:NewsArticle[]     = await getNewsArticles(countryCode)
+    const { countryCode } = await props.params;
+    const newsArticleList:NewsArticle[]    = await getNewsArticles(countryCode)
     const headlineMetadata:CountryHeadline = await getHeadlineInfo(countryCode)
 
     let newsArticleElements: ReactNode[] = []
     await Promise.all(newsArticleList.map(
         (val, idx, arr) => {
-            const element = getNewsArticleElement(val, 'None')
+            const element = (<NewsItem newsArticle={val} noImageUrl="https://example.com/empty.png"></NewsItem>);
             newsArticleElements.push(element)
         }
     ))
