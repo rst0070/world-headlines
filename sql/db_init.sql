869b74aad3ebbcf5fb0f6f0f33037fd3fe23638d
Wonbin Kim <kwb0711@gmail.com>
2024-12-13 23:46:33 +0900

    Migrate DB to postgresql

--- sql/db_init.sql
+++ sql/db_init.sql
@@ -1,112 +1,95 @@
----------------------- DATABASE
-----------------------
-IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'world_headlines')
+-- Create DATABASE
+CREATE EXTENSION IF NOT EXISTS dblink;
+
+DO $$
 BEGIN
-	CREATE DATABASE world_headlines
-END;
-USE world_headlines;
+	IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'world_headlines') THEN
+		PERFORM dblink_exec('dbname=postgres', 'CREATE DATABASE world_headlines');
+	END IF;
+END $$;
+
+\c world_headlines;
 ---------------------- Create TABLE
 ----------------------
 
-IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='HEADLINE' AND xtype='U')
-BEGIN
-	CREATE TABLE HEADLINE (
-		country_code nvarchar(2) primary key,
-		country_name nvarchar(60), -- longest eng name of country is 56 long. 4 more character, in case.
-		url nvarchar(MAX),
-		last_update datetime2
-	)
-END;
+CREATE TABLE IF NOT EXISTS HEADLINE(
+	country_code  char(2) primary key,
+	country_name  text,
+	url text,
+	last_update timestamp
+);
 
-IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='HEADLINE_ARTICLES' AND xtype='U')
-BEGIN
-	CREATE TABLE HEADLINE_ARTICLES (
-		row_id int IDENTITY(1, 1) primary key,
-		country_code nvarchar(2) not null,
-		url nvarchar(MAX),
-		title nvarchar(MAX),
-		description nvarchar(MAX),
-		image_url nvarchar(MAX),
-		publish_date datetime2,
-		source nvarchar(MAX),
-		foreign key (country_code) references HEADLINE(country_code)
-	)
-END;
+CREATE TABLE IF NOT EXISTS HEADLINE_ARTICLES(
+	country_code char(2),
+	url text,
+	title text,
+	description text,
+	image_url text,
+	publish_date timestamp,
+	source text,
+	primary key (country_code, url),
+	foreign key (country_code) references HEADLINE(country_code)
+)PARTITION BY LIST(country_code);
 
-IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='CRAWLED_ARTICLES' AND xtype='U')
-BEGIN
-	CREATE TABLE CRAWLED_ARTICLES (
-		row_id int IDENTITY(1, 1) primary key,
-		country_code nvarchar(2) not null,
-		url nvarchar(MAX),
-		title nvarchar(MAX),
-		description nvarchar(MAX),
-		image_url nvarchar(MAX),
-		publish_date datetime2,
-		source nvarchar(MAX),
-		foreign key (country_code) references HEADLINE(country_code)
-	)
-END;
+CREATE TABLE IF NOT EXISTS CRAWLED_ARTICLES(
+	country_code char(2),
+	url text,
+	title text,
+	description text,
+	image_url text,
+	publish_date timestamp,
+	source text,
+	primary key (country_code, url),
+	foreign key (country_code) references HEADLINE(country_code)
+)PARTITION BY LIST(country_code);
 
-IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='ARCHIVED_ARTICLES' AND xtype='U')
-BEGIN
-	CREATE TABLE ARCHIVED_ARTICLES (
-		row_id int IDENTITY(1, 1) primary key,
-		country_code nvarchar(2) not null,
-		url nvarchar(MAX),
-		title nvarchar(MAX),
-		description nvarchar(MAX),
-		image_url nvarchar(MAX),
-		publish_date datetime2,
-		source nvarchar(MAX),
-		foreign key (country_code) references HEADLINE(country_code)
-	)
-END;
+
 ---------------------- Create User & Grant permission
 ----------------------
-
-IF NOT EXISTS (SELECT * FROM master.sys.server_principals WHERE name = 'wh_updater')
+DO
+$$
 BEGIN
-    CREATE LOGIN [wh_updater] WITH PASSWORD = N'WorldHeadlinesUpdater99!'
-END;
+    IF NOT EXISTS (SELECT 1 FROM pg_catalog.pg_roles WHERE rolname = 'wh_updater') THEN
+        CREATE USER wh_updater WITH PASSWORD 'wh_updater';
+    END IF;
+END
+$$;
 
-IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = N'wh_updater')
-BEGIN
-    CREATE USER [wh_updater] FOR LOGIN [wh_updater]
-END;
+GRANT 
+	INSERT,SELECT,UPDATE 
+ON
+	HEADLINE --,ARCHIVED_ARTICLES
+TO 
+	wh_updater;
 
-GRANT INSERT,SELECT,UPDATE ON OBJECT::dbo.HEADLINE TO wh_updater;
-GRANT INSERT,SELECT,UPDATE ON OBJECT::dbo.ARCHIVED_ARTICLES TO wh_updater;
-GRANT INSERT,SELECT,UPDATE,DELETE ON OBJECT::dbo.HEADLINE_ARTICLES TO wh_updater;
-GRANT INSERT,SELECT,UPDATE,DELETE ON OBJECT::dbo.CRAWLED_ARTICLES TO wh_updater;
+GRANT 
+	INSERT,SELECT,UPDATE,DELETE 
+ON
+	HEADLINE_ARTICLES, CRAWLED_ARTICLES
+TO 
+	wh_updater;
 ---------------------- Insert data into HEADLINE
 ----------------------
 -- check below for language code
 -- https://cloud.google.com/translate/docs/languages?hl=ko
 
 INSERT INTO HEADLINE (country_name, country_code, url)
-    VALUES ('United States', 'us', 'https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFZxYUdjU0FtVnVHZ0pWVXlnQVAB?ceid=US:en&oc=3');
-INSERT INTO HEADLINE (country_name, country_code, url)
-    VALUES ('China', 'cn', 'https://news.google.com/rss/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRFZxYUdjU0JYcG9MVU5PR2dKRFRpZ0FQAQ?ceid=CN:zh-Hans&oc=3');
-INSERT INTO HEADLINE (country_name, country_code, url)
-    VALUES ('India', 'in', 'https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFZxYUdjU0FtaHBHZ0pKVGlnQVAB?ceid=IN:hi&oc=3');
-INSERT INTO HEADLINE (country_name, country_code, url)
-    VALUES ('German', 'de', 'https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFZxYUdjU0FtUmxHZ0pFUlNnQVAB?ceid=DE:de&oc=3');
-INSERT INTO HEADLINE (country_name, country_code, url)
-    VALUES ('Franch', 'fr', 'https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFZxYUdjU0FtWnlHZ0pHVWlnQVAB?ceid=FR:fr&oc=3');
-INSERT INTO HEADLINE (country_name, country_code, url)
-    VALUES ('Japan', 'jp', 'https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFZxYUdjU0FtcGhHZ0pLVUNnQVAB?ceid=JP:ja&oc=3');
-INSERT INTO HEADLINE (country_name, country_code, url)
-    VALUES ('Brasil', 'br', 'https://news.google.com/rss/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRFZxYUdjU0JYQjBMVUpTR2dKQ1VpZ0FQAQ?ceid=BR:pt-419&oc=3&hl=pt-BR&gl=BR');
-INSERT INTO HEADLINE (country_name, country_code, url)
-    VALUES ('Russia', 'ru', 'https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFZxYUdjU0FuSjFHZ0pTVlNnQVAB?ceid=RU:ru&oc=3');
-INSERT INTO HEADLINE (country_name, country_code, url)
-    VALUES ('Korea', 'kr', 'https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFZxYUdjU0FtdHZHZ0pMVWlnQVAB?ceid=KR:ko&oc=3');
-INSERT INTO HEADLINE (country_name, country_code, url)
-    VALUES ('United Kingdom', 'gb', 'https://news.google.com/rss/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRFZxYUdjU0JXVnVMVWRDR2dKSFFpZ0FQAQ?ceid=GB:en&oc=3');
-INSERT INTO HEADLINE (country_name, country_code, url)
-    VALUES ('Taiwan', 'tw', 'https://news.google.com/rss/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRFZxYUdjU0JYcG9MVlJYR2dKVVZ5Z0FQAQ?ceid=TW:zh-Hant&oc=3');
-INSERT INTO HEADLINE (country_name, country_code, url)
-	VALUES ('Israel', 'il', 'https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFZxYUdjU0FtbDNHZ0pKVENnQVAB?ceid=IL:he&oc=3');
-INSERT INTO HEADLINE (country_name, country_code, url)
-	VALUES ('Lebanon', 'lb', 'https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFZxYUdjU0FtRnlHZ0pNUWlnQVAB?ceid=LB:ar&oc=3');
+VALUES 
+	('United States', 'us', 'https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFZxYUdjU0FtVnVHZ0pWVXlnQVAB?ceid=US:en&oc=3'),
+	('Korea', 'kr', 'https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFZxYUdjU0FtdHZHZ0pMVWlnQVAB?ceid=KR:ko&oc=3');
+	-- ('China', 'cn', 'https://news.google.com/rss/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRFZxYUdjU0JYcG9MVU5PR2dKRFRpZ0FQAQ?ceid=CN:zh-Hans&oc=3'),
+	-- ('India', 'in', 'https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFZxYUdjU0FtaHBHZ0pKVGlnQVAB?ceid=IN:hi&oc=3'),
+	-- ('German', 'de', 'https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFZxYUdjU0FtUmxHZ0pFUlNnQVAB?ceid=DE:de&oc=3'),
+	-- ('Franch', 'fr', 'https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFZxYUdjU0FtWnlHZ0pHVWlnQVAB?ceid=FR:fr&oc=3'),
+	-- ('Japan', 'jp', 'https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFZxYUdjU0FtcGhHZ0pLVUNnQVAB?ceid=JP:ja&oc=3'),
+	-- ('Brasil', 'br', 'https://news.google.com/rss/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRFZxYUdjU0JYQjBMVUpTR2dKQ1VpZ0FQAQ?ceid=BR:pt-419&oc=3&hl=pt-BR&gl=BR'),
+	-- ('Russia', 'ru', 'https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFZxYUdjU0FuSjFHZ0pTVlNnQVAB?ceid=RU:ru&oc=3'),
+	-- ('United Kingdom', 'gb', 'https://news.google.com/rss/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRFZxYUdjU0JXVnVMVWRDR2dKSFFpZ0FQAQ?ceid=GB:en&oc=3'),
+	-- ('Taiwan', 'tw', 'https://news.google.com/rss/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRFZxYUdjU0JYcG9MVlJYR2dKVVZ5Z0FQAQ?ceid=TW:zh-Hant&oc=3'),
+	-- ('Israel', 'il', 'https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFZxYUdjU0FtbDNHZ0pKVENnQVAB?ceid=IL:he&oc=3'),
+	-- ('Lebanon', 'lb', 'https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFZxYUdjU0FtRnlHZ0pNUWlnQVAB?ceid=LB:ar&oc=3');
+
+CREATE TABLE CRAWLED_ARTICLES_us PARTITION OF CRAWLED_ARTICLES FOR VALUES IN ('us');
+CREATE TABLE CRAWLED_ARTICLES_kr PARTITION OF CRAWLED_ARTICLES FOR VALUES IN ('kr');
+CREATE TABLE HEADLINE_ARTICLES_us PARTITION OF HEADLINE_ARTICLES FOR VALUES IN ('us');
+CREATE TABLE HEADLINE_ARTICLES_kr PARTITION OF HEADLINE_ARTICLES FOR VALUES IN ('kr');
