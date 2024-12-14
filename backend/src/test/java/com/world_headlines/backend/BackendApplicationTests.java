6bfb718e638f4ffc3c1a3a3dacdd12c18c19df25
Wonbin Kim <kwb0711@gmail.com>
2024-12-14 18:14:41 +0900

    Add script: dev run

--- backend/src/test/java/com/world_headlines/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/world_headlines/backend/BackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.world_headlines.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // activates application-test.properties
 class BackendApplicationTests {
 
 	@Test
